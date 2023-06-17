e6bd676eb870eed80251cbf3b28e6f39ddb9b167
Raphael2b3 <raphael.schuetz0311@gmail.com>
2023-06-17 11:13:47 +0200

    implemented multiplikativ inverses

---
+++ diskreteMathematik/stochastik/mittel.py
@@ -0,0 +1,7 @@
+def arethmetischesmittel(liste):
+    print("Aretmetisches Mittel")
+    print(liste)
+    out = sum(liste)/len(liste)
+    print(out)
+    return out
+
---
+++ diskreteMathematik/stochastik/tests.py
@@ -0,0 +1,26 @@
+import mittel
+
+# Teste die Güte
+
+# TODO Zufallsgröße
+# TODO Verteiliungsfunktion
+
+def güte(liste):
+    """
+    m = û : Mühhut
+
+    1. Wähle konfidenzniveau nahe bei 1
+
+    2. Betrachte die zu x_ gehörige standardnormalverteilung zufallsgröße
+    u = (x_-m)/(q/sqrt(n))
+
+
+    für parameter Tetta
+
+    :return:
+    """
+
+    x_ = mittel.arethmetischesmittel(liste)
+
+    konfidenz_niveau = 0.95
+
